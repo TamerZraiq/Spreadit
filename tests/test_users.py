62df57b9164003d553f28c3f117887f48bf8b00b
Yi Zheng <g00425075@atu.ie>
2025-10-10 15:33:53 +0100

    Implemented tests on user login, tests failed as the login endpoint is not expecting a JSON body

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,14 +1,18 @@
 import pytest
 
-#Dummy user payload for testing
+#Payload for testing User
 def user_payload(id = 1, name = "Bob", email = "Bob@atu.ie", username = "LilBob", pw = "bob123", cid = 1,  year = 1, uid = "g00425076"):
     return {"id": id, "user_id": uid, "name": name, "email": email, "username": username, "password": pw, "course_id": cid, "year": year}
 
-#Dummy user-signup payload for testing
+#Payload for testing sign up
 def user_signup_payload(name = "Bob", email = "Bob@atu.ie", username = "LilBob", pw = "bob123", cid = 1,  year = 1, uid = "g00425076"):
     return {"user_id": uid, "name": name, "email": email, "username": username, "password": pw, "course_id": cid, "year": year}
 
-#Test for the /sign-up endpoint
+#Payload for testing log in
+def user_login_payload(email = "Bob@atu.ie", pw = "bob123"):
+    return {"email" : email, "password" : pw}
+
+#This tests when signup is successful
 def test_signup_ok(client): 
     r = client.post("/sign-up", json=user_signup_payload()) 
     assert r.status_code == 201 
@@ -29,18 +33,37 @@ def test_duplicate_user_id_conflict(client):
     assert r.status_code == 409 
     assert "user id already exists" in r.json()["detail"].lower() 
 
-    #This tests when duplicate email occurs when signing up
+#This tests when duplicate email occurs when signing up
 def test_duplicate_email_conflict(client): 
     client.post("/sign-up", json=user_signup_payload(email = "Bob@atu.ie")) 
     r = client.post("/sign-up", json=user_signup_payload(uid = "g00425077", email = "Bob@atu.ie")) 
     assert r.status_code == 409 
     assert "email already exists" in r.json()["detail"].lower() 
 
-    #This tests when duplicate username occurs when signing up
+#This tests when duplicate username occurs when signing up
 def test_duplicate_username_conflict(client): 
     client.post("/sign-up", json=user_signup_payload(username = "LilBob")) 
     r = client.post("/sign-up", json=user_signup_payload(uid = "g00425077", email = "Bobi@atu.ie", username = "LilBob"))
     assert r.status_code == 409 
     assert "username already exists" in r.json()["detail"].lower() 
 
+#This tests when login is successful
+def test_login_ok(client):
+    client.post("/sign-up", json=user_signup_payload())
+    r = client.post("/login", json=user_login_payload())
+    assert r.status_code == 200
+    assert "login successful" in r.json()["message"].lower() 
 
+#This tests when login with wrong password
+def test_login_wrong_password(client):
+    client.post("/sign-up", json=user_signup_payload())
+    r = client.post("/login", json=user_login_payload(pw = "Wrongpassword"))
+    assert r.status_code == 401
+    assert "invalid email or password" in r.json()["detail"].lower() 
+
+#This tests when login with email that doesn't exist
+def test_login_wrong_email(client):
+    client.post("/sign-up", json=user_signup_payload())
+    r = client.post("/login", json=user_login_payload(email = "wrongemail@atu.ie"))
+    assert r.status_code == 401
+    assert "invalid email or password" in r.json()["detail"].lower() 
