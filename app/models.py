d757b92bc471214acdb8d022e4fa3bc2c2afc4aa
David <118185346+DavidJ7705@users.noreply.github.com>
2025-10-23 12:46:21 +0000

    Set up SQLite Database connection with our schemas and new model for db. Added create and read methods which uses db connection. Must now change our old code to use the db

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, Integer
+
+class Base(DeclarativeBase):
+    pass
+
+
+class UserDB(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    user_id: Mapped[str] = mapped_column(String, nullable=False)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
+    username: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
+    password: Mapped[str] = mapped_column(String, unique=False, nullable=False)
+    course_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
